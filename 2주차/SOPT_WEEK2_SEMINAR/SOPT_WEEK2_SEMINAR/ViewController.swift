e4ab194d27363663f6e0b9c5e07303ecd8888c9c
qodhrkawk <gabriel1120@naver.com>
2020-04-26 15:57:50 +0900

    2주차 세미나 실습 추가

---
+++ 2주차/SOPT_WEEK2_SEMINAR/SOPT_WEEK2_SEMINAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_WEEK2_SEMINAR
+//
+//  Created by Yunjae Kim on 2020/04/25.
+//  Copyright © 2020 Yunjae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
